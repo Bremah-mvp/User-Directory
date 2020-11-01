bf23036bfb1622df751c4c0764bc29bc483da323
jackie <mvpbremah@yahoo.com>
2020-11-01 05:42:10 -0500

    commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Employeesd from './components/Employeesd';
+
+
+function App() {
+  return (
+    <Container>
+      <Employeesd />
+    </Container>
+  );
+}
+
+export default App;
